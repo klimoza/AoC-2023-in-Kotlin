f6afdeda2ab2b07ccf308b526d27d3620954d02c
klimoza <i220303@mail.ru>
2023-12-18 19:16:34 +0100

    Add day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,36 +1,63 @@
 import java.util.LinkedList
 import java.util.Stack
+import kotlin.math.abs
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        var ans = 0
+    fun expandGalaxies(input: List<String>, coef: Long): Long {
         val n = input.size
         val m = input[0].length
-        val place = MutableList<Int>(m) { 0 }
-        for(i in input.indices){
-            for(j in input[0].indices) {
+        val rows = MutableList(n) { false }
+        val cols = MutableList(m) { false }
+        for(i in input.indices) {
+            for(j in input.indices) {
                 if(input[i][j] == '#') {
-                    place[j] = i + 1
-                } else if(input[i][j] == 'O') {
-                    ans += n - place[j]
-                    place[j]++
+                    rows[i] = true
+                    cols[j] = true
                 }
+            }
+        }
+        val c = mutableListOf<Pair<Long, Long>>()
+        var cur_i = 0.toLong()
+        for(i in rows.indices) {
+            var cur_j = 0.toLong()
+            for(j in cols.indices) {
+                if(input[i][j] == '#')
+                    c.add(Pair(cur_i, cur_j))
+                cur_j++
+                if(!cols[j])
+                    cur_j += coef - 1
+            }
+            cur_i++
+            if(!rows[i]) {
+                cur_i += coef - 1
+            }
+        }
+        var ans = 0.toLong()
+        for(i in c.indices) {
+            for(j in i+1 until c.size) {
+                val (x1, y1) = c[i]
+                val (x2, y2) = c[j]
+                ans += abs(x1 - x2) + abs(y1 - y2)
             }
         }
         return ans
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): Long {
+        return expandGalaxies(input, 2.toLong())
+    }
+
+    fun part2(input: List<String>): Long {
+        return expandGalaxies(input, 1e6.toLong())
     }
 
     val testInput1 = readInput("Day11_example1")
-    check (part1(testInput1) == 136)
+    check (part1(testInput1) == 374.toLong())
+    check(expandGalaxies(testInput1, 10.toLong()) == 1030.toLong())
+    check(expandGalaxies(testInput1, 100.toLong()) == 8410.toLong())
 
-//    val testInput2 = readInput("Day11_example2")
-//    check(part2(testInput2) == 8)
 
     val input = readInput("Day11")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
---
+++ src/Day14.kt
@@ -0,0 +1,33 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var ans = 0
+        val n = input.size
+        val m = input[0].length
+        val place = MutableList<Int>(m) { 0 }
+        for(i in input.indices){
+            for(j in input[0].indices) {
+                if(input[i][j] == '#') {
+                    place[j] = i + 1
+                } else if(input[i][j] == 'O') {
+                    ans += n - place[j]
+                    place[j]++
+                }
+            }
+        }
+        return ans
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput1 = readInput("Day14_example1")
+    check (part1(testInput1) == 136)
+
+//    val testInput2 = readInput("Day14_example2")
+//    check(part2(testInput2) == 8)
+
+    val input = readInput("Day14")
+    part1(input).println()
+//    part2(input).println()
+}
